72e61fea2e9cf8975c0995295c2f1a85f1b60d66
dreaming-panda <64370186+dreaming-panda@users.noreply.github.com>
2024-06-07 21:39:24 -0400

    Create execute.sh

---
+++ execute.sh
@@ -0,0 +1,21 @@
+#use your todo folder below
+cd /data/home/beidic/zhuoming/submit-meta
+git pull
+
+cd todo
+
+for script in *.sh; do
+   bash "$script"
+done
+
+
+wait
+
+
+mv *.sh ../submitted
+
+
+cd ..
+git add -A .
+git commit -a -m "submit jobs"
+git push -u origin main
